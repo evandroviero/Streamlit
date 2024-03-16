d74c9ec44c26a2a0c49cb8b199ac9c11e847dba3
evandroviero <evandro.viero@gmail.com>
2024-03-16 13:54:47 -0300

    deixando apenas em função

--- analise_financeira.py
+++ analise_financeira.py
@@ -4,49 +4,52 @@ import numpy as np
 
 st.set_page_config(layout="wide",page_title="Relatorio de Vendas", page_icon="📈")
 
-class Dashboard():
-    def __init__(self) -> None:
-        self.file = 'data/sales.csv'
+
+file = 'data/sales.csv'
     
-    @st.cache_data
-    def load_csv(self):
-        df = pd.read_csv(self.file)
-        df["Order Date"] = pd.to_datetime(df["Order Date"])
-        df["Year"] = df["Order Date"].dt.year.astype(str)
-        return df
-    
-    def side_bar(self, df):
-        with st.sidebar:
-            st.title("Relatorio de Vendas")
-            distinct_regions = df["Region"].unique().tolist()
-            region_selected = st.multiselect("Região", distinct_regions)
-            item_type = st.multiselect("Item",df["Item Type"].unique().tolist())
-            if region_selected:
-                df = df.loc[df["Region"].isin(region_selected)]
-            if item_type:
-                df = df.loc[df["Item Type"].isin(item_type)]
-        return df
-    
-    def line_chart(self, df):
-        df = df.groupby('Year')['Total Profit'].sum().reset_index()
-        return df
-        
+@st.cache_data
+def load_csv():
+    df = pd.read_csv(file)
+    return df
+
+def ajusta_coluna(df):
+    df["Order Date"] = pd.to_datetime(df["Order Date"])
+    df["Year"] = df["Order Date"].dt.year.astype(str)
+    return df
+
+def side_bar(df):
+    with st.sidebar:
+        st.title("Relatorio de Vendas")
+        distinct_regions = df["Region"].unique().tolist()
+        region_selected = st.multiselect("Região", distinct_regions)
+        item_type = st.multiselect("Item",df["Item Type"].unique().tolist())
+        if region_selected:
+            df = df.loc[df["Region"].isin(region_selected)]
+        if item_type:
+            df = df.loc[df["Item Type"].isin(item_type)]
+    return df
+
+def line_chart(df):
+    df = df.groupby('Year')['Total Profit'].sum().reset_index()
+    return df
     
 
-    def graphic(self, df):
-        st.write("Lucro por País")
-        st.bar_chart(df, x="Country", y="Total Profit")
-        line = self.line_chart(df)
-        st.line_chart(line, x='Year',y='Total Profit')
-        # st.pyplot(self.graphic_pie(df))
-    
 
-    def run_dashboard(self):
-        df = self.load_csv()
-        df = self.side_bar(df)
-        self.graphic(df)
-        st.write(df)
-        # st.dataframe(df_line, use_container_width=True)
+def graphic(df):
+    st.write("Lucro por País")
+    st.bar_chart(df, x="Country", y="Total Profit")
+    line = line_chart(df)
+    st.line_chart(line, x='Year',y='Total Profit')
+    # st.pyplot(graphic_pie(df))
+
+
+def run_dashboard():
+    df = load_csv()
+    df = ajusta_coluna(df)
+    df = side_bar(df)
+    graphic(df)
+    st.write(df)
+    # st.dataframe(df_line, use_container_width=True)
 
 if __name__ == "__main__":
-    Dashboard().run_dashboard()
+    run_dashboard()
