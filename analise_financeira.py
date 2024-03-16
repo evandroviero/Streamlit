adfc0918f6dc9aa582fd98673457264fca312ed1
evandroviero <66703748+evandroviero@users.noreply.github.com>
2024-03-16 12:58:45 -0300

    Update analise_financeira.py

--- analise_financeira.py
+++ analise_financeira.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_title="Relatorio de Vendas", page_icon="📈")
 
 class Dashboard():
     def __init__(self) -> None:
@@ -11,12 +11,12 @@ class Dashboard():
     def load_csv(self):
         df = pd.read_csv(self.file)
         df["Order Date"] = pd.to_datetime(df["Order Date"])
-        df["Year"] = df["Order Date"].dt.year
+        df["Year"] = df["Order Date"].dt.year.astype(str)
         return df
     
     def side_bar(self, df):
         with st.sidebar:
-            st.title("Análise de Lucro")
+            st.title("Relatorio de Vendas")
             distinct_regions = df["Region"].unique().tolist()
             region_selected = st.multiselect("Região", distinct_regions)
             item_type = st.multiselect("Item",df["Item Type"].unique().tolist())
@@ -26,29 +26,26 @@ class Dashboard():
                 df = df.loc[df["Item Type"].isin(item_type)]
         return df
     
-    def graphic_pie(self, df):
+    def line_chart(self, df):
         df = df.groupby('Year')['Total Profit'].sum().reset_index()
-        labels = df['Year'].tolist()
-        sizes = df['Total Profit'].tolist()
-        fig, ax = plt.subplots(facecolor='none')
-        ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-        ax.axis('equal')
-        ax.legend(labels, loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-        return fig
+        return df
+        
     
 
     def graphic(self, df):
         st.write("Lucro por País")
         st.bar_chart(df, x="Country", y="Total Profit")
-        st.pyplot(self.graphic_pie(df))
+        line = self.line_chart(df)
+        st.line_chart(line, x='Year',y='Total Profit')
+        # st.pyplot(self.graphic_pie(df))
     
 
     def run_dashboard(self):
         df = self.load_csv()
         df = self.side_bar(df)
         self.graphic(df)
-        st.write("Database")
-        st.dataframe(df, use_container_width=True)
+        st.write(df)
+        # st.dataframe(df_line, use_container_width=True)
 
 if __name__ == "__main__":
     Dashboard().run_dashboard()
